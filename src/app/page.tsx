134f3781060fc4f367ad2571217c0f2cf0c8cd33
Fraimer De La Cruz <77998859+fraimerr@users.noreply.github.com>
2025-02-08 23:10:23 -0400

    Add styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
 	return (
-		<div>
-			<h1>Study Notes Gen from PDF</h1>
+		<div className="flex flex-col h-screen w-screen items-center justify-center py-6">
+			<h1 className="text-4xl font-bold mb-6">Study Notes Gen from PDF</h1>
+			<form className="flex flex-col items-center mb-6">
+				<label
+					htmlFor="file"
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				>
+					Select a file
+				</label>
+			</form>
 		</div>
 	);
 }
